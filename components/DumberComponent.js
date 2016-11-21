4ec5db353664d7f9b23a3a4b692af75ad1056fb7
krissasantos <krissa.santos31@gmail.com>
2016-11-20 20:29:47 -0500

    Done.

---
+++ components/DumberComponent.js
@@ -0,0 +1,13 @@
+const React = require('react');
+// class DumberComponent extends React.Component {
+ 
+export default function DumberComponent(props) {
+  // render(){
+    return(
+      <div onClick={props.handleClick}>I am just happy</div>
+      )
+  // }
+
+}
+
+module.exports = DumberComponent;
